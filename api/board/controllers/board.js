2d241acd977cbe11a036020105ef53716c70a199
Shokhjakhon Komiljonov <komiljonovshohjahon@gmail.com>
2021-05-20 17:22:41 +0900

    Backend customization for password checking and /passChecker implemneted

--- api/board/controllers/board.js
+++ api/board/controllers/board.js
@@ -1,4 +1,6 @@
-"use strict";
+const { sanitizeEntity } = require("strapi-utils");
+
+("use strict");
 
 /*
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
@@ -14,7 +16,31 @@ module.exports = {
         { id: board.id },
         { views: parseInt(board.views) + 1 }
       );
-      return board;
+
+      // return board;
+      return sanitizeEntity(board, { model: strapi.models.board });
+    } catch (error) {
+      console.error(error);
+      return ctx.send({
+        success: false,
+      });
+    }
+  },
+
+  async findOneAndPassCheck(ctx) {
+    const { id } = ctx.params;
+    try {
+      const board = await strapi.services.board.findOne({ id });
+
+      // BEFORE PRODUCTION DO NOT FORGET TO REMOVE THE BELOW LINE
+      console.log(board.password);
+      // BEFORE PRODUCTION DO NOT FORGET TO REMOVE THE ABOVE LINE
+
+      if (ctx.request.body === board.password) {
+        return sanitizeEntity(board, { model: strapi.models.board });
+      } else {
+        return false;
+      }
     } catch (error) {
       console.error(error);
       return ctx.send({
@@ -23,3 +49,61 @@ module.exports = {
     }
   },
 };
+
+// async findOneAndUpdate(ctx) {
+//   const { id } = ctx.params;
+//   try {
+//     const board = await strapi.services.board.findOne({ id });
+//     await strapi.services.board.update(
+//       { id: board.id },
+//       { views: parseInt(board.views) + 1 }
+//     );
+//     const new_board = {
+//       title: board.title,
+//       name: board.name,
+//       email: board.email,
+//       number: board.number,
+//       message: board.message,
+//       private: board.private,
+//       reply: board.reply,
+//       published_at: board.published_at,
+//       created_at: board.created_at,
+//       updated_at: board.updated_at,
+//     };
+//     return new_board;
+//   } catch (error) {
+//     console.error(error);
+//     return ctx.send({
+//       success: false,
+//     });
+//   }
+// },
+//   async find(ctx) {
+//     try {
+//       const board = await strapi.services.board.find();
+//       var new_board = [];
+//       board.map((item) =>
+//         new_board.push({
+//           id: item.id,
+//           title: item.title,
+//           name: item.name,
+//           email: item.email,
+//           views: item.views,
+//           number: item.number,
+//           message: item.message,
+//           private: item.private,
+//           reply: item.reply,
+//           published_at: item.published_at,
+//           created_at: item.created_at,
+//           updated_at: item.updated_at,
+//         })
+//       );
+//       return new_board;
+//     } catch (error) {
+//       console.error(error);
+//       return ctx.send({
+//         success: false,
+//       });
+//     }
+//   },
+// };
--- config/middleware.js
+++ config/middleware.js
@@ -1,14 +1,15 @@
-module.exports = {
-  settings: {
-    cors: {
-      enabled: true, // tried true and false
-      headers: "*",
-      origin: [
-        "http://localhost:5500",
-        "http://google.com",
-        "http://localhost:1337",
-        "http://127.0.0.1:5500",
-      ],
-    },
-  },
-};
+// module.exports = {
+//   load: {
+//     before: ["csp", "xss"],
+//     order: ["csp", "xss"],
+//     after: ["parser", "router"],
+//   },
+//   settings: {
+//     csp: {
+//       enabled: false,
+//     },
+//     xss: {
+//       enabled: false,
+//     },
+//   },
+// };
