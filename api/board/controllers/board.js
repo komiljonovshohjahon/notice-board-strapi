89daf81c99f1cdcae1ad71f6592d62a748d05bb1
Shokhjakhon Komiljonov <komiljonovshohjahon@gmail.com>
2021-05-21 15:11:53 +0900

    xss applied to all methods

--- api/board/controllers/board.js
+++ api/board/controllers/board.js
@@ -41,6 +41,42 @@ module.exports = {
     }
   },
 
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    const options = {};
+
+    const sanitized = {
+      name: xss(ctx.request.body.name, options),
+      password: xss(ctx.request.body.password, options),
+      email: xss(ctx.request.body.email, options),
+      number: xss(ctx.request.body.number, options),
+      title: xss(ctx.request.body.title, options),
+      message: xss(ctx.request.body.message, options),
+    };
+
+    try {
+      if (ctx.is("multipart")) {
+        const { data, files } = parseMultipartData(ctx);
+        entity = await strapi.services.board.update({ id }, data, {
+          files,
+        });
+      } else {
+        entity = await strapi.services.board.update(
+          { id },
+          ctx.request.body,
+          copyer(sanitized, ctx.request.body)
+        );
+
+        data.created_by = entity.name;
+      }
+    } catch (error) {
+      return error;
+    }
+  },
+
   async findOneAndUpdate(ctx) {
     const { id } = ctx.params;
     try {
@@ -50,9 +86,8 @@ module.exports = {
         { views: parseInt(board.views) + 1 }
       );
 
-      // console.log(board.password);
+      console.log(board.password);
 
-      // return board;
       return sanitizeEntity(board, { model: strapi.models.board });
     } catch (error) {
       console.error(error);
@@ -65,14 +100,22 @@ module.exports = {
   async findOneAndPassCheck(ctx) {
     const { id } = ctx.params;
 
+    const options = {};
+
+    const sanitized = {
+      password: xss(ctx.request.body, options),
+    };
+
     try {
       const board = await strapi.services.board.findOne({ id });
 
       // BEFORE PRODUCTION DO NOT FORGET TO REMOVE THE BELOW LINE
-      console.log(board.password);
+      // console.log(board.password);
       // BEFORE PRODUCTION DO NOT FORGET TO REMOVE THE ABOVE LINE
 
       if (ctx.request.body === board.password) {
+        board.password = sanitized.password;
+
         return sanitizeEntity(board, { model: strapi.models.board });
       } else {
         return false;
@@ -85,61 +128,3 @@ module.exports = {
     }
   },
 };
-
-// async findOneAndUpdate(ctx) {
-//   const { id } = ctx.params;
-//   try {
-//     const board = await strapi.services.board.findOne({ id });
-//     await strapi.services.board.update(
-//       { id: board.id },
-//       { views: parseInt(board.views) + 1 }
-//     );
-//     const new_board = {
-//       title: board.title,
-//       name: board.name,
-//       email: board.email,
-//       number: board.number,
-//       message: board.message,
-//       private: board.private,
-//       reply: board.reply,
-//       published_at: board.published_at,
-//       created_at: board.created_at,
-//       updated_at: board.updated_at,
-//     };
-//     return new_board;
-//   } catch (error) {
-//     console.error(error);
-//     return ctx.send({
-//       success: false,
-//     });
-//   }
-// },
-//   async find(ctx) {
-//     try {
-//       const board = await strapi.services.board.find();
-//       var new_board = [];
-//       board.map((item) =>
-//         new_board.push({
-//           id: item.id,
-//           title: item.title,
-//           name: item.name,
-//           email: item.email,
-//           views: item.views,
-//           number: item.number,
-//           message: item.message,
-//           private: item.private,
-//           reply: item.reply,
-//           published_at: item.published_at,
-//           created_at: item.created_at,
-//           updated_at: item.updated_at,
-//         })
-//       );
-//       return new_board;
-//     } catch (error) {
-//       console.error(error);
-//       return ctx.send({
-//         success: false,
-//       });
-//     }
-//   },
-// };
